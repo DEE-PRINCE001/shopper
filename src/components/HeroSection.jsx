73a2136ddef6833fc320446089bbec142e9d7cf8
Ajani Honour <ajanihonour@gmail.com>
2026-07-24 18:52:20 +0100

    Implemented the Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
+import electronics from "/images/fashion.png"
 
 const HeroSection = () => {
-  return (
-    <div className='bg-secondary w-full'>
-        <div className='flex flex-col md:flex-row'>
-            <div className='w-full md:w-1/2 flex flex-col'>
-            <div className='text-4xl font-extrabold'>Everything You <br/> Need, All in <br/> One Place</div>
-            </div>
-            <div className='w-full md:w-1/2 aspect-square'>
-            <div className='w-full h-full rounded-full'>
-                
-            </div>
+    return (
+        <div className='bg-secondary w-full py-10 px-5 md:px-10'>
+            <div className='flex text-primary flex-col md:flex-row'>
+                <div className='w-full md:w-1/2 flex flex-col'>
+                    <div className='text-5xl font-extrabold'>EVERYTHING <br /> YOU NEED, ALL <br />IN ONE PLACE</div>
+                </div>
+                <div className='flex flex-col items-center w-full md:w-1/2'>
+                    <div className='w-full h-full rounded-full'>
+                        <img src={electronics} />
+                    </div>
+
+                    <div>
+                        <div className='px-7 font-semibold text-center w-fit rounded-full border-2 py-2 border-primary'>Fashion</div>
+                    </div>
+
+                </div>
 
             </div>
-
         </div>
-    </div>
 
-    
-  )
+
+    )
 }
 
 export default HeroSection
