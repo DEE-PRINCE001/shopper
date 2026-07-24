3dccdd2569f09226b1458d5319c58f730401c4cb
Ajani Honour <ajanihonour@gmail.com>
2026-07-24 15:32:51 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+
+const App = () => {
+  return (
+
+    <BrowserRouter>
+      <div className='font-lupio font-bold'>App</div>
+    </BrowserRouter>
+  )
+}
+
+export default App
